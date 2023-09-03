bb417b9973819a2be9872defe49465cf2d38cd4a
elkinmadrid <elkinamp@gmail.com>
2023-09-02 23:45:23 -0500

    Variables de entorno

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from api.routes.user_route import users_bp
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
 # settings
-app.secret_key = ''
-app.config["SQLALCHEMY_DATABASE_URI"] = ''
+
+host = os.environ['HOST']
+nombre_bd = os.environ['NAME_BD']
+nombre_usuario = os.environ['USERNAME']
+password = os.environ['PASSWORD']
+
+app.config["SQLALCHEMY_DATABASE_URI"] = f'postgresql://{nombre_usuario}:{password}@{host}:5432/{nombre_bd}'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # no cache
